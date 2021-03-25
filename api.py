8c3b94824701290c3c8af678d6f0579d4f2d6988
SergeyBoRss <66353172+SergeyBoRss@users.noreply.github.com>
2021-03-25 11:33:27 +0300

    PrApi

--- api.py
+++ api.py
@@ -1,12 +1,16 @@
 import flask
+import os
 
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
+
+port= int(os.environ.get('PORT', 5000))
 
 @app.route('/', methods=['GET'])
 def index():
     return '<h1>INDEX</h1>'
 
 app.run(
-    host='127.0.0.1'
+    host='0.0.0.0',
+    port=port
 )
